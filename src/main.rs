ba8634964d864a12b87b54d8986e61663194db78
Sean Brickley <sean@wabr.io>
2019-11-27 15:05:57 -0500

    Added tests. GOL Behavior now works.

--- src/main.rs
+++ src/main.rs
@@ -35,44 +35,83 @@ impl Grid {
         Ok(state)
     }
 
-    // THE FUNCTION BELOW IS REALLY UGLY. FIND A WAY TO WRAP TO THE OTHER SIDE
-    // WHEN INDEXING NEAR THE EDGE OF THE GRID. MAKE SURE IT DOESN'T
-    // LOOK LIKE SHIT
-    /*
-        fn count_neighbors(&self, x: usize, y: usize) -> u32 {
-            let mut nieghbors = 0
+    fn glider() -> GameResult<Grid> {
+        let mut cells: Vec<Vec<Cell>> = vec![];
 
-            match (x, y) {
-                (0, 0) =>,
-                (self.grid.len(), self.grid[0].len()) =>,
-                (self.grid.len(), 0) =>,
-                (0, self.grid[0].len()) =>,
-                (0, y) =>,
-                (self.grid.len(), y) =>,
-                (x, 0) =>,
-                (x, self.grid[0].len()) =>,
-                _ => {
-                    for row_mod in -1..=1 {
-                        for col_mod in -1..=1 {
-                            if self.cells[x + row_mod][y + col_mod].alive {
-                                neighbors += 1
-                            }
-                        }
-                    }
+        for _ in 0..=(RIGHT / CELL_SIZE) as usize {
+            let mut row = vec![];
+
+            for _ in 0..=(BOTTOM / CELL_SIZE) as usize {
+                row.push(Cell { alive: false });
+            }
+
+            cells.push(row);
+        }
+
+        cells[10][20].alive = true;
+        cells[11][20].alive = true;
+        cells[12][20].alive = true;
+        cells[12][21].alive = true;
+        cells[11][22].alive = true;
+
+        let state = Grid { cells };
+        Ok(state)
+    }
+
+    fn bar() -> GameResult<Grid> {
+        let mut cells: Vec<Vec<Cell>> = vec![];
+
+        for _ in 0..=(RIGHT / CELL_SIZE) as usize {
+            let mut row = vec![];
+
+            for _ in 0..=(BOTTOM / CELL_SIZE) as usize {
+                row.push(Cell { alive: false });
+            }
+
+            cells.push(row);
+        }
+
+        cells[10][20].alive = true;
+        cells[10][19].alive = true;
+        cells[10][18].alive = true;
+
+        let state = Grid { cells };
+        Ok(state)
+    }
+
+    fn count_neighbors(&self, x: i32, y: i32) -> u32 {
+        let mut neighbors = 0;
+
+        for row_mod in -1i32..=1i32 {
+            for col_mod in -1i32..=1i32 {
+                if row_mod == 0 && col_mod == 0 { continue }
+                let (wrapped_x, wrapped_y) = wrap_coords(x + row_mod, y + col_mod);
+                if self.cells[wrapped_x][wrapped_y].alive {
+                    neighbors += 1
                 }
             }
         }
-    */
+
+        neighbors
+    }
 }
 
 impl event::EventHandler for Grid {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.cells[30][30].alive = true;
-        self.cells[31][30].alive = true;
-        self.cells[32][30].alive = true;
-        self.cells[32][29].alive = true;
-        self.cells[31][28].alive = true;
+        let mut new_grid = Grid::new()?;
 
+        for row in 0..self.cells.len() {
+            for col in 0..self.cells[row].len() {
+                match self.count_neighbors(row as i32, col as i32) {
+                    0|1 => new_grid.cells[row][col].alive = false,
+                    2 => new_grid.cells[row][col].alive =  self.cells[row][col].alive,
+                    3 => new_grid.cells[row][col].alive = true,
+                    4..=8 => new_grid.cells[row][col].alive = false,
+                    _ => println!("FUCK")
+                }
+            }
+        }
+        self.cells = new_grid.cells;
         Ok(())
     }
 
@@ -109,6 +148,37 @@ impl event::EventHandler for Grid {
 fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("super_simple", "ggez");
     let (ctx, event_loop) = &mut cb.build()?;
-    let grid = &mut Grid::new()?;
+    let grid = &mut Grid::glider()?;
     event::run(ctx, event_loop, grid)
+}
+
+fn wrap_coords(mut x: i32, mut y: i32) -> (usize, usize) {
+    if x > (RIGHT / CELL_SIZE) as i32 {
+        x = 0;
+    } else if x < 0 {
+        x = (RIGHT / CELL_SIZE) as i32;
+    }
+
+    if y > (BOTTOM / CELL_SIZE) as i32 {
+        y = 0;
+    } else if y < 0 {
+        y = (BOTTOM / CELL_SIZE) as i32;
+    }
+
+    (x as usize, y as usize)
+}
+
+#[test]
+fn test_wrap() {
+    assert_eq!(wrap_coords(100, 100), (0, 0));
+    assert_eq!(wrap_coords(-100, -100), (79, 59));
+}
+
+#[test]
+fn test_neighbor_count() {
+    let grid = Grid::bar().unwrap();
+
+    let neighbors = grid.count_neighbors(10, 19);
+
+    assert_eq!(neighbors, 2u32);
 }
